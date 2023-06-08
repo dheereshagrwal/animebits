2cb8bdf9ac37fef53a1cdfdb84537dbe1a9beb21
kmarahar <da11@iitbbs.ac.in>
2023-06-09 03:49:54 +0530

    removed csrf and try except

--- website/views.py
+++ website/views.py
@@ -19,43 +19,48 @@ def post_login(sender, user, request, **kwargs):
 
     cart_id = request.session.get("cart_id")
 
-    cart = Cart.objects.get(cart_id=cart_id)
+    try:
+        cart = Cart.objects.get(cart_id=cart_id)
 
-    cart_items = CartItem.objects.filter(cart=cart)
-    product_variations = []
-    for cart_item in cart_items:
-        variations = cart_item.variations.all()
-        product_variations.append(list(variations))
+        cart_items = CartItem.objects.filter(cart=cart)
+        product_variations = []
+        for cart_item in cart_items:
+            variations = cart_item.variations.all()
+            product_variations.append(list(variations))
 
-    # get the cart items from the user who logged in
-    user_cart_items = CartItem.objects.filter(user=user, is_active=True)
-    existing_variations_list = []
-    id_list = []
-    for user_cart_item in user_cart_items:
-        existing_variations = user_cart_item.variations.all()
-        existing_variations_list.append(list(existing_variations))
-        id_list.append(user_cart_item.id)
+        # get the cart items from the user who logged in
+        user_cart_items = CartItem.objects.filter(user=user, is_active=True)
+        existing_variations_list = []
+        id_list = []
+        for user_cart_item in user_cart_items:
+            existing_variations = user_cart_item.variations.all()
+            existing_variations_list.append(list(existing_variations))
+            id_list.append(user_cart_item.id)
 
-    for product_variation in product_variations:
-        if product_variation in existing_variations_list:
-            index = existing_variations_list.index(product_variation)
-            item = CartItem.objects.get(id=id_list[index])
-            item.quantity += 1
-            item.user = user
-            item.save()
-        else:
-            # for cart_item in cart_items:
-            #     cart_item.user = user
-            #     cart_item.save()
-            index = product_variations.index(product_variation)
-            item = CartItem.objects.get(id=cart_items[index].id)
-            item.user = user
-            item.save()
+        for product_variation in product_variations:
+            if product_variation in existing_variations_list:
+                index = existing_variations_list.index(product_variation)
+                item = CartItem.objects.get(id=id_list[index])
+                item.quantity += 1
+                item.user = user
+                item.save()
+            else:
+                # for cart_item in cart_items:
+                #     cart_item.user = user
+                #     cart_item.save()
+                index = product_variations.index(product_variation)
+                item = CartItem.objects.get(id=cart_items[index].id)
+                item.user = user
+                item.save()
+    except:
+        pass
 
 
 # Create your views here.
 def home(request):
-    products = Product.objects.all().filter(is_available=True).order_by("-created_date")[:10]
+    products = (
+        Product.objects.all().filter(is_available=True).order_by("-created_date")[:10]
+    )
     products_count = products.count()
     context = {"products": products, "products_count": products_count}
     return render(request, "home.html", context)

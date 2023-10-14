d640c6bb2c5af89c15e6005076349b0131bc377f
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-10-14 21:40:19 +0530

    urls update

--- website/settings.py
+++ website/settings.py
@@ -124,6 +124,7 @@ CACHES = {
     }
 }
 import dj_database_url
+
 DATABASES = {"default": dj_database_url.parse(config("DB_URL"))}
 
 # Password validation
--- website/urls.py
+++ website/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
